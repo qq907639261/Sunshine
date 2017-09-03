e9e826d10b662683b0a3f472e4c16781177f604e
Qinzl_XHBB <qinzl.xhbb@qq.com>
2017-09-03 15:41:41 +0800

    更新启动图标和应用名。 更新activity_main布局。 静态添加测试数据。

--- app/src/main/java/com/xhbb/qinzl/sunshine/MainActivity.java
+++ app/src/main/java/com/xhbb/qinzl/sunshine/MainActivity.java
@@ -1,13 +1,25 @@
 package com.xhbb.qinzl.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWeatherTextView = (TextView) findViewById(R.id.weatherTextView);
+
+        for (int i = 0; i < 100; i++) {
+            mWeatherTextView.append("晴 22°");
+            if (i < 99) {
+                mWeatherTextView.append("\n");
+            }
+        }
     }
 }
