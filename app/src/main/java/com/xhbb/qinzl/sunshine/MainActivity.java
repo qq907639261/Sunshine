5739479cde8e3e99a0518a86da3984ee7a92a505
Qinzl_XHBB <qinzl.xhbb@qq.com>
2017-09-05 10:40:12 +0800

    MainActivity 新增下拉刷新

--- app/src/main/java/com/xhbb/qinzl/sunshine/MainActivity.java
+++ app/src/main/java/com/xhbb/qinzl/sunshine/MainActivity.java
@@ -1,33 +1,40 @@
 package com.xhbb.qinzl.sunshine;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.xhbb.qinzl.sunshine.data.PreferencesUtils;
-import com.xhbb.qinzl.sunshine.server.JsonObjects;
+import com.xhbb.qinzl.sunshine.server.JsonObjects.WeatherObject;
 import com.xhbb.qinzl.sunshine.server.NetworkUtils;
 
 public class MainActivity extends AppCompatActivity
-        implements Response.Listener<JsonObjects.WeatherObject>, Response.ErrorListener {
+        implements Response.Listener<WeatherObject>, Response.ErrorListener,
+        SwipeRefreshLayout.OnRefreshListener {
 
     private static final String TAG = "MainActivity";
 
     private TextView mWeatherTextView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
         mWeatherTextView = (TextView) findViewById(R.id.weatherTextView);
 
         addWeatherLocationNetworkRequest();
+        mSwipeRefreshLayout.setOnRefreshListener(this);
     }
 
     private void addWeatherLocationNetworkRequest() {
+        NetworkUtils.cancelAllRequests(this, TAG);
+
         String weatherLocation = PreferencesUtils.getWeatherLocation(this);
         NetworkUtils.addWeatherLocationRequest(this, weatherLocation, this, this, TAG);
     }
@@ -40,11 +47,16 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onErrorResponse(VolleyError error) {
+        mSwipeRefreshLayout.setRefreshing(false);
+        mWeatherTextView.setText(R.string.networking_failed_text);
     }
 
     @Override
-    public void onResponse(JsonObjects.WeatherObject response) {
-        for (JsonObjects.WeatherObject.Body.Weather weather : response.showapi_res_body.dayList) {
+    public void onResponse(WeatherObject response) {
+        mSwipeRefreshLayout.setRefreshing(false);
+        mWeatherTextView.setText("");
+
+        for (WeatherObject.Body.Weather weather : response.showapi_res_body.dayList) {
             mWeatherTextView.append(weather.daytime + "\n");
             mWeatherTextView.append(weather.day_weather + "\n");
             mWeatherTextView.append(weather.day_air_temperature + "\n");
@@ -52,5 +64,10 @@ public class MainActivity extends AppCompatActivity
             mWeatherTextView.append(weather.day_wind_direction + "\n");
             mWeatherTextView.append(weather.day_weather_pic + "\n\n");
         }
+    }
+
+    @Override
+    public void onRefresh() {
+        addWeatherLocationNetworkRequest();
     }
 }
